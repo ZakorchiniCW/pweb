311b16f4ea8f68ea57c24231b24ddd77aaf6383f
ZakorchiniCW <37644938+ZakorchiniCW@users.noreply.github.com>
2018-09-27 03:36:36 -0300

    Update script_ex4.js
    Update script_ex4

--- script_ex4.js
+++ script_ex4.js
@@ -1,40 +1,39 @@
- function maior(a, b, c) {
-            if(a>c && b>c){
-                if(a>b)
-                    return a;
-                else if(a==b){
-                    alert("Primeiro e segundo são iguais e maiores");
+function maior(a, b, c) {
+	if(a>b && a>c)
+		return a;
+	else if(b>a && b>c)
+		return b;
+	else if(c>b && c>a)
+		return c;
+	else if(a==b && a>c){
+		alert("Primeiro e segundo são iguais e maiores");
                 return a;}
-                else
-                    return b;
-                }
-            if(a>b && c>b){
-                if(a>c)
-                    return a;
-                else if(a==c){
-                   alert("Primeiro e terceiro são iguais e maiores");
+	else if(a==c && a>b){
+		alert("Primeiro e terceiro são iguais e maiores");
                 return a;}
-                else
-                    return c;
-            }
-            if(b>a && c>a){
-                if(b>c)
-                    return b;
-                else if(b==c){
-                    alert("Segundo e terceiro são iguais e maiores");
+	else if(b==c && b>a){
+		alert("Segundo e terceiro são iguais e maiores");
                 return b;}
-                else
-                    return c;
-            }
- }
-      
+	else
+		return a;
+}
+		
+     
 var a, b, c, d;
 
+do{
 a= parseFloat(prompt("digite primeiro valor" ));
 b= parseFloat(prompt("digite segundo valor" ));
 c= parseFloat(prompt("digite terceiro valor" ));
 
-        d = maior(a,b,c);
-        alert("Maior é "+d);
+        if(a==b && a==c && b==c){
+		alert("Os três são iguais! ");}
+	else{
+		d = maior(a,b,c);
+        	alert("Maior é "+ d);}
+	var escolha=confirm("Deseja jogar denovo?")
+	if(escolha==0)
+		alert("Até logo!");
+}while(escolha);
      
         
